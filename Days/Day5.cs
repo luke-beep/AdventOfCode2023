1c5ededde8de9a9ddef93b7af2cd46f56953160d
LukeHjo <lukehjo@gmail.com>
2023-12-05 23:54:20 +0100

    feat: Part 5.

--- Days/Day5.cs
+++ Days/Day5.cs
@@ -1,4 +1,6 @@
-﻿using AdventOfCode2023.Days.Contracts;
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using AdventOfCode2023.Days.Contracts;
 using AdventOfCode2023.Utilities;
 
 namespace AdventOfCode2023.Days;
@@ -13,13 +15,147 @@ public class Day5 : ISolution
     }
     public string GetPath() => _path;
 
-    public Task<Dictionary<string, string>> Solve()
+    public async Task<Dictionary<string, string>> Solve()
     {
+        var fileService = new FileService();
+        var input = await fileService.ReadAllLinesAsync(_path);
+        return await Solution(input);
+    }
+
+    private Task<Dictionary<string, string>> Solution(string[] input)
+    {
+        Almanac almanac = new();
+
+        var seeds = input[0];
+        var seedList = seeds.Replace("seeds: ", "");
+        var seedListSplit = seedList.Split(" ");
+        var seedListInt = seedListSplit.Select(Int128.Parse).ToList();
+        
+        almanac.Seeds = seedListInt;
+
+        List<Int128[]> currentMap = null;
+
+
+        foreach (var line in input.Skip(1))
+        {
+            if (line.StartsWith("seed-to-soil map:"))
+            {
+                currentMap = almanac.SeedToSoilMap;
+            }
+            else if (line.StartsWith("soil-to-fertilizer map:"))
+            {
+                currentMap = almanac.SoilToFertilizerMap;
+            }
+            else if (line.StartsWith("fertilizer-to-water map:"))
+            {
+                currentMap = almanac.FertilizerToWaterMap;
+            }
+            else if (line.StartsWith("water-to-light map:"))
+            {
+                currentMap = almanac.WaterToLightMap;
+            }
+            else if (line.StartsWith("light-to-temperature map:"))
+            {
+                currentMap = almanac.LightToTemperatureMap;
+            }
+            else if (line.StartsWith("temperature-to-humidity map:"))
+            {
+                currentMap = almanac.TemperatureToHumidityMap;
+            }
+            else if (line.StartsWith("humidity-to-location map:"))
+            {
+                currentMap = almanac.HumidityToLocationMap;
+            }
+            else if (!string.IsNullOrWhiteSpace(line) && currentMap != null)
+            {
+                var numbers = line.Split(' ').Select(Int128.Parse).ToArray();
+                currentMap.Add(numbers);
+            }
+        }
+
+        var lowest = GetLowestLocation(almanac);
         Dictionary<string, string> result = new()
         {
-            { "Part 1", "1" },
+            { "Part 1", lowest.ToString() },
             { "Part 2", "2" }
         };
         return Task.FromResult(result);
+    }
+
+    public Int128 GetLowestLocation(Almanac almanac)
+    {
+        var locationNumbers = new List<Int128>();
+        foreach (var seed in almanac.Seeds)
+        {
+            var soil = MapNumber(almanac.SeedToSoilMap, seed);
+            var fertilizer = MapNumber(almanac.SoilToFertilizerMap, soil);
+            var water = MapNumber(almanac.FertilizerToWaterMap, fertilizer);
+            var light = MapNumber(almanac.WaterToLightMap, water);
+            var temperature = MapNumber(almanac.LightToTemperatureMap, light);
+            var humidity = MapNumber(almanac.TemperatureToHumidityMap, temperature);
+            var location = MapNumber(almanac.HumidityToLocationMap, humidity);
+            locationNumbers.Add(location);
+        }
+        return locationNumbers.Min();
+    }
+
+    public Int128 MapNumber(List<Int128[]> map, Int128 number)
+    {
+        if (map == null || map.Count == 0)
+        {
+            return number;
+        }
+
+        foreach (var mapping in map)
+        {
+            if (number >= mapping[1] && number < mapping[1] + mapping[2])
+            {
+                return mapping[0] + (number - mapping[1]);
+            }
+        }
+        return number;
+    }
+}
+
+public class Almanac
+{
+    public List<Int128> Seeds
+    {
+        get; set;
+    }
+
+    public List<Int128[]> SeedToSoilMap
+    {
+        get; set;
+    }
+
+    public List<Int128[]> SoilToFertilizerMap
+    {
+        get; set;
+    }
+
+    public List<Int128[]> FertilizerToWaterMap
+    {
+        get; set;
+    }
+
+    public List<Int128[]> WaterToLightMap
+    {
+        get; set;
+    }
+
+    public List<Int128[]> LightToTemperatureMap
+    {
+        get; set;
+    }
+
+    public List<Int128[]> TemperatureToHumidityMap
+    {
+        get; set;
+    }
+
+    public List<Int128[]> HumidityToLocationMap
+    {
+        get; set;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections;
+using System.Reflection;
 using AdventOfCode2023.Days;
 using AdventOfCode2023.Days.Contracts;
 using AdventOfCode2023.Utilities;
@@ -8,62 +9,98 @@ namespace AdventOfCode2023;
 
 internal class Program
 {
+    private const string Path1 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day1.txt";
+    private const string Path2 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day2.txt";
+    private const string Path3 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day3.txt";
+    private const string Path4 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day4.txt";
+    private const string Path5 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day5.txt";
+
+    private static readonly ISolution Day1 = new Day1(Path1);
+    private static readonly ISolution Day2 = new Day2(Path2);
+    private static readonly ISolution Day3 = new Day3(Path3);
+    private static readonly ISolution Day4 = new Day4(Path4);
+    private static readonly ISolution Day5 = new Day5(Path5);
+
+    private static readonly Table PathTable = new();
+    private static readonly Table RootTable = new();
+
+    private static readonly Calendar AdventCalendar = new(2023, 12);
+
+    private static readonly Panel AdventCalendarPanel = new(Align.Center(AdventCalendar));
+    private static readonly Panel PathPanel = new(Align.Center(PathTable));
+    private static readonly Panel RootPanel = new(Align.Center(RootTable));
+
+    private static readonly List<int> Ticked = new();
+
     private static async Task Main(string[] args)
     {
-        AnsiConsole.Write(new FigletText("Advent of Code 2023").Color(Color.Green).Centered());
-        AnsiConsole.Write(new Markup("[link]https://github.com/luke-beep/AdventOfCode2023[/]").Centered());
-        AnsiConsole.Background = Color.Black;
+        await InitializeConsoleAsync();
+        await ConfigurePanelsAsync();
+        await InitializeTablesAsync();
+        await ConfigureTablesAsync();
+        await ConfigureCalendarAsync();
+        await InitializeLayoutAsync();
+    }
 
-        var path1 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day1.txt";
-        var path2 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day2.txt";
-        var path3 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day3.txt";
-        var path4 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day4.txt";
-        var path5 = @"F:\source\luke-beep\AdventOfCode2023\Inputs\Day5.txt";
+    private static async Task InitializeLayoutAsync()
+    {
+        await InitializeConsoleAsync();
 
-        ISolution day1 = new Day1(path1);
-        ISolution day2 = new Day2(path2);
-        ISolution day3 = new Day3(path3);
-        ISolution day4 = new Day4(path4);
-        ISolution day5 = new Day5(path5);
+        var layout = new Layout("Root")
+            .SplitColumns(
+                new Layout("Left"),
+                new Layout("Right").SplitColumns(new Layout("Top"), new Layout("Bottom")));
 
-        var solutions = new List<ISolution> { day1, day2, day3, day4, day5 };
 
-        // Tables
-        var pathTable = new Table
-        {
-            Border = TableBorder.Double,
-            BorderStyle = null,
-            UseSafeBorder = false,
-            ShowHeaders = false,
-            ShowRowSeparators = false,
-            ShowFooters = false,
-            Expand = false,
-            Width = null,
-            Title = null,
-            Caption = null
-        };
-        pathTable.AddColumn("Paths");
 
-        // Root
-        var root = new Table();
+        layout["Left"].Update(RootPanel);
+        layout["Right"]["Top"].Update(AdventCalendarPanel);
+        layout["Right"]["Bottom"].Update(PathPanel);
+
+        AnsiConsole.Write(layout);
+    }
+
+    private static async Task ConfigurePanelsAsync()
+    {
+        RootPanel.Header = new PanelHeader("Solutions");
+        RootPanel.Border = BoxBorder.Double;
+        RootPanel.UseSafeBorder = false;
+        RootPanel.Expand = false;
+
+        PathPanel.Header = new PanelHeader("Paths");
+        PathPanel.Border = BoxBorder.Double;
+        PathPanel.UseSafeBorder = false;
+        PathPanel.Expand = false;
+
+        AdventCalendarPanel.Header = new PanelHeader("Calender");
+        AdventCalendarPanel.Border = BoxBorder.Double;
+        AdventCalendarPanel.UseSafeBorder = false;
+        AdventCalendarPanel.Expand = false;
+
+        await Task.CompletedTask;
+    }
+
+    private static async Task InitializeTablesAsync()
+    {
+        var solutions = await PromptSolutions();
+
+        PathTable.AddColumn("Paths");
 
         List<Text> columns = new();
         List<Text> rows = new();
 
-        // Solve all solutions
-        var lastSolution = 1;
         foreach (var solution in solutions)
         {
             var solve = await solution.Solve();
             var solutionName = solution.GetType().GetCustomAttribute<Solution>()?.Day;
             var path = new TextPath(solution.GetPath());
-            pathTable.AddRow(path);
+            PathTable.AddRow(path);
             if (solutionName != null)
             {
-                lastSolution = solutionName.Value;
+                Ticked.Add(solutionName.Value);
                 foreach (var kvp in solve)
                 {
-                    columns.Add(new Text($"Day {solutionName} | {kvp.Key}"));
+                    columns.Add(new Text($"Day {solutionName}, {kvp.Key}"));
                     rows.Add(new Text(kvp.Value));
                 }
             }
@@ -72,75 +109,101 @@ internal class Program
                 throw new Exception("Solution name is null");
             }
         }
-        columns.ForEach(s => root.AddColumn(new TableColumn(s)));
-        root.AddRow(rows);
-
-
-        // Calender
-        var calender = new Calendar(2023, 12)
-        {
-            Day = 0,
-            Border = TableBorder.Double,
-            UseSafeBorder = false,
-            BorderStyle = null,
-            Culture = null,
-            HighlightStyle = Color.Red,
-            ShowHeader = true,
-            HeaderStyle = null
-        }.AddCalendarEvent(2023, 12, lastSolution).HideHeader().Alignment(Justify.Center);
-
-
-        // Panels
-        var calenderPanel = new Panel(Align.Center(calender))
-        {
-            Header = new PanelHeader("Calender", Justify.Center),
-            Border = BoxBorder.Double,
-            UseSafeBorder = false,
-            BorderStyle = null,
-            Expand = false,
-            Padding = null,
-            Height = null,
-            Width = null
-        };
-        var pathPanel = new Panel(Align.Center(pathTable))
-        {
-            Header = new PanelHeader("Paths", Justify.Right),
-            Border = BoxBorder.Double,
-            UseSafeBorder = false,
-            BorderStyle = null,
-            Expand = false,
-            Padding = null,
-            Height = null,
-            Width = null
-
-        };
-        var rootPanel = new Panel(Align.Center(new Columns(root)))
-        {
-            Header = new PanelHeader("Solutions"),
-            Border = BoxBorder.Double,
-            UseSafeBorder = false,
-            BorderStyle = null,
-            Expand = false,
-            Padding = null,
-            Height = null,
-            Width = null
-        };
-
-        // Layout
-        var layout = new Layout("Root")
-            .SplitColumns(
-                new Layout("Left"),
-                new Layout("Right").SplitColumns(new Layout("Top"), new Layout("Bottom")));
-
-
-
-        layout["Left"].Update(rootPanel);
-        layout["Right"]["Top"].Update(calenderPanel);
-        layout["Right"]["Bottom"].Update(pathPanel);
-        
-        AnsiConsole.Write(layout);
+        columns.ForEach(s => RootTable.AddColumn(new TableColumn(s)));
+        RootTable.AddRow(rows);
     }
 
- 
+    private static async Task ConfigureTablesAsync()
+    {
+        RootTable.Border = TableBorder.Double;
+        RootTable.Expand = false;
 
+        PathTable.Border = TableBorder.Double;
+        PathTable.Expand = false;
+
+        await Task.CompletedTask;
+    }
+
+    private static async Task ConfigureCalendarAsync()
+    {
+        AdventCalendar.Border = TableBorder.Double;
+        AdventCalendar.HighlightStyle = new Style(Color.Red);
+
+        foreach(var i in Ticked)
+        {
+            AdventCalendar.AddCalendarEvent(2023, 12, i).HideHeader().Alignment(Justify.Center);
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private static Task<List<ISolution>> PromptSolutions()
+    {
+        var solutionList = new List<string>
+        {
+            "Day 1",
+            "Day 2",
+            "Day 3",
+            "Day 4",
+            "Day 5"
+        };
+
+        var solutionStrings = AnsiConsole.Prompt(
+            new MultiSelectionPrompt<string>()
+                .Title("Which [green]Advent of Code[/] solutions would you like to see?")
+                .PageSize(10)
+                .Required()
+                .MoreChoicesText("[grey](Move up and down to reveal more solutions)[/]")
+                .InstructionsText(
+                    "[grey](Press [blue]<space>[/] to toggle a solution, " +
+                    "[green]<enter>[/] to accept)[/]")
+                .AddChoices(solutionList));
+
+        var solutions = new List<ISolution>();
+        foreach (var solutionString in solutionStrings)
+        {
+            switch (solutionString)
+            {
+                case "Day 1":
+                    solutions.Add(Day1);
+                    break;
+                case "Day 2":
+                    solutions.Add(Day2);
+                    break;
+                case "Day 3":
+                    solutions.Add(Day3);
+                    break;
+                case "Day 4":
+                    solutions.Add(Day4);
+                    break;
+                case "Day 5":
+                    solutions.Add(Day5);
+                    break;
+            }
+        }
+        return Task.FromResult(solutions);
+    }
+
+    private static async Task InitializeConsoleAsync()
+    {
+        AnsiConsole.Clear();
+
+        AnsiConsole.Write(new FigletText("Advent of Code 2023").Color(Color.Green).Centered());
+        AnsiConsole.Write(new Markup("[link]https://github.com/luke-beep/AdventOfCode2023[/]").Centered());
+        AnsiConsole.Write(new Markup("[link]https://adventofcode.com/2023[/]").Centered());
+
+        AnsiConsole.Background = Color.Black;
+
+        await WriteEmptyLinesAsync(4);
+    }
+
+    private static async Task WriteEmptyLinesAsync(int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            AnsiConsole.WriteLine();
+        }
+
+        await Task.CompletedTask;
+    }
 }
